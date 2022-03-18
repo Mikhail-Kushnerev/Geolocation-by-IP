6ef92189d52e70e383807c082f9c3f42aa45c210
Mikhail <mikushnerev@yandex.ru>
2022-03-18 21:53:07 +0300

    добавлены папки для удобной сортировки по типам

--- script.py
+++ script.py
@@ -20,13 +20,13 @@ def get_info_by_ip(ip):
         if os.path.isfile(f"data/{location}.txt"):
             print('Файл с таким именем уже создан.')
         else:
-            
+
             with open(f"data/{location}.txt", mode='a', encoding='utf-8') as f:
                 for target, info in data.items():
                     f.write(f'{target} : {info}\n')
 
             area = folium.Map(location=[response.get('lat'), response.get('lon')])
-            area.save(f"{response.get('query')}_{location}.html")
+            area.save(f"data/html/{response.get('query')}_{location}.html")
             print('Файл успешно создан.')
 
     except requests.exceptions.ConnectionError:
